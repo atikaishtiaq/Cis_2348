e05bf5c3e21804ae09a63b5f152a25a7e0a71dba
CIS 2348 <atikaishtiaq@Yahoo.com>
2019-09-11 14:24:45 -0500

    made a small change

---
+++ HelloWorld2.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+    //comment
+    public static void main(String{} args) {
+        System.out.printIn("Hello, World");
+        System.out.printIn(1+5);
+        System.out.print("What happens now? ");
+        System.out.print("...and now?");
+    }
+
+}
---
+++ balance.java
@@ -0,0 +1,18 @@
+public class balance {
+        //commit
+public static void main( String[]args ) {
+        int balance = 100;
+        int amount = 0;
+        String Text = "";
+        while ( balance > 0 ) (
+                text ="Your balance: \u0024" + balance + "." ;
+                text += "\nAmount to withdraw:";
+
+                System.out.printIn(text);
+                // User Input
+                Scanner input = new Scanner(System.in);
+                amount = inptut .nextInt();
+                balance -= amount;
+}
+
+}
