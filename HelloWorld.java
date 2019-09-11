dd2e4a9c7b0977ba88e66c9d3df12757a1e9c71b
CIS 2348 <atikaishtiaq@Yahoo.com>
2019-09-11 14:08:54 -0500

    initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+
+/*
+ *  THIS is the class HelloWorld. It is just used for presenting comments and print outs.
+    */
+public class Hello World (
+        public static void main (String[] args) {
+
+
+            // this is my first print statement
+        system.out.printin("Hello, World!");
+
+        // this will print the result of the sum 1 + 5
+        system.out.printin (1+5);
+        System.out.print("What happens now");
+        System.out.print("...and now?");
+        }
+
+        )
+        )
